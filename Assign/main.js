99e8eb1a671a8c5654ecefa3cc850d5fc5e6408f
Sachin13579 <97518055+Sachin13579@users.noreply.github.com>
2022-03-02 23:44:47 +0530

    Add files via upload

---
+++ Assign/main.js
@@ -0,0 +1,11 @@
+const add = require('./add.js');
+console.log(add(6,5));
+
+const div = require('./divide')
+console.log(div(10,5))
+
+const multi=require("./multiply")
+console.log(multi(4,2));
+
+const sub=require("./subtract")
+console.log(sub (10,5));
