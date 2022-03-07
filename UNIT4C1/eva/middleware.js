052a3a4976639aa6e54d83263706a70391dc3e88
Sachin Singh <97518055+Sachin13579@users.noreply.github.com>
2022-03-07 13:10:45 +0530

    Add files via upload

--- UNIT4C1/eva/middleware.js
+++ UNIT4C1/eva/middleware.js
@@ -1,44 +1,43 @@
-const express=require("express")
-const app=express()
 
-app.use(logger)
+const express = require("express");
+const app = express();
 
+app.use(logger);
 
 app.get("/books",(req,res)=>{
-    res.send({route:"/books"})
-})
-
-// app.use(checkPermission)
-app.get("/libraries",(req,res)=>{
-   return res.send({route:"/libraries"})
-})
+    res.send({ route: "/books"})
+});
 
 
-app.get("/authors",(req,res)=>{
-   return res.send({route:"/authors",role:req.role})
-})
+app.get("/libraries",checkPermission("librarian"),(req,res)=>{
+   return res.send({ route: "/libraries",permission:res.permission});
+});
 
-app.listen(8000,()=>{
-   console.log("I am listeining")
-})
-
+app.get("/authors",checkPermission("author"),(req,res)=>{
+    return res.send({ route: "/authors",permission:res.permission});
+});
 
 function logger(req,res,next){
-    console.log (req.path)
-    next()
+    console.log(req.path);
+    next();
+};
+function checkPermission(role){
+    return function (req,res,next){
+        if(role === "librarian"){
+            res.permission = true;
+            return next();
+        }else if(role === "author"){
+            res.permission = true;
+            return next();
+        }
+        else{
+            return res.send("not allowed")
+
+        }
+    }
 }
 
 
-
-function checkPermission( req,res,next){
-if (req.path==="/libraries"){
-    req.role("true")
-}
-else if (req.path==="/authors"){
-    req.role ("true")
-}
-else{
-    console.log("NOT allowed")
-}
-return res.send("No allowed")
-}    
+app.listen(5000,()=>{
+    console.log("hiii");
+})
