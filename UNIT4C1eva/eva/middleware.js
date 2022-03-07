afc162ed31c57af9d9ccc1fe04d0b038d119f0c0
Sachin Singh <97518055+Sachin13579@users.noreply.github.com>
2022-03-07 13:13:52 +0530

    Add files via upload

---
+++ UNIT4C1eva/eva/middleware.js
@@ -0,0 +1,43 @@
+
+const express = require("express");
+const app = express();
+
+app.use(logger);
+
+app.get("/books",(req,res)=>{
+    res.send({ route: "/books"})
+});
+
+
+app.get("/libraries",checkPermission("librarian"),(req,res)=>{
+   return res.send({ route: "/libraries",permission:res.permission});
+});
+
+app.get("/authors",checkPermission("author"),(req,res)=>{
+    return res.send({ route: "/authors",permission:res.permission});
+});
+
+function logger(req,res,next){
+    console.log(req.path);
+    next();
+};
+function checkPermission(role){
+    return function (req,res,next){
+        if(role === "librarian"){
+            res.permission = true;
+            return next();
+        }else if(role === "author"){
+            res.permission = true;
+            return next();
+        }
+        else{
+            return res.send("not allowed")
+
+        }
+    }
+}
+
+
+app.listen(8000,()=>{
+    console.log("I am listening");
+})
